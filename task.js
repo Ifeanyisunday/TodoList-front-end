f0861020c85d1b0276b659e4853b362b60e4ac2c
Ifeanyisunday <sundayjnr10@gmail.com>
2024-08-19 04:10:29 +0100

    first front end

--- task.js
+++ task.js
@@ -1,16 +1,11 @@
-document.querySelector("#addBtn").addEventListener('click', (e) => {
-    const popup = document.querySelector("#pop-up")
-    popup.style.opacity = "1";
 
-})
-
-const newform = document.querySelector("#input-form");
+const newform = document.querySelector("#pop-up");
 newform.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const title = document.getElementById("#task-title").value;
-    const note = document.getElementById("#task-note").value;
-    const priority = document.getElementById("#priority").value;
-    const email = document.getElementById("#user-email").value;
+    const title = document.getElementById("task-title").value;
+    const note = document.getElementById("task-note").value;
+    const priority = document.getElementById("priority").value;
+    const email = document.getElementById("user-email").value;
 
     const data = await fetch('http://localhost:8081/api/v1/task/addTask', {
         method: 'POST',
@@ -28,9 +23,17 @@ newform.addEventListener("submit", async (e) => {
         const createList = document.createElement("li")
         ulTag.appendChild(createList);
         createList.textContent = `${title}, ${note}, ${priority}}`;
+        const popup = document.querySelector("#pop-up")
+        popup.style.opacity = "0";
     }else{
         alert("something went wrong")
     }
+
+})
+
+document.querySelector("#addBtn").addEventListener('click', (e) => {
+    const popup = document.querySelector("#pop-up")
+    popup.style.opacity = "1";
 
 })
 
